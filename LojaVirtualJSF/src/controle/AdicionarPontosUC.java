4156b6e49f1ee59a7ab9b7a845151bb912d036bf
tiago.bonatti <tiago.bonatti@9c4e91d4-ccfa-27a7-cf8f-361c695ca832>
2010-09-25 01:19:43 +0000

    Loja Virtual em JSF

---
+++ LojaVirtualJSF/src/controle/AdicionarPontosUC.java
@@ -0,0 +1,52 @@
+package controle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import remote.GerenciadorPontosRemote;
+
+@SessionScoped
+@ManagedBean(name="AdicionarPontosUC")
+public class AdicionarPontosUC {
+
+	private String cpf;
+	private int pontos;
+	
+	public AdicionarPontosUC() {}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getPontos() {
+		return pontos;
+	}
+	public void setPontos(int pontos) {
+		this.pontos = pontos;
+	}
+	
+    public String salvar() throws Exception{
+    	
+    	try {
+	        
+    		Context ctx = new InitialContext();
+	        GerenciadorPontosRemote gerenciador = (GerenciadorPontosRemote) ctx.lookup("java:global/TrabalhoFinal/ProgramaFidelidadeEJB/GerenciadorPontosUC");
+	        gerenciador.acumular(cpf, pontos);
+	        
+	        return "formAdicionarPontos";
+	        
+		} finally {
+			
+		}    	
+    }
+	
+    public String cancelar(){
+        return "formAdicionarPontos";
+   }
+	
+}
