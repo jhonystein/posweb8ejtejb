64004bde1195f167589f991e431460de0b1230fb
jhonystein <jhonystein@9c4e91d4-ccfa-27a7-cf8f-361c695ca832>
2010-10-02 21:49:37 +0000

    Atualizacoes

--- ExtratoClienteJSF/src/controle/ExtratoUC.java
+++ ExtratoClienteJSF/src/controle/ExtratoUC.java
@@ -1,10 +1,103 @@
 package controle;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.xml.rpc.ServiceException;
+
+import remote.ExtratoPontosUC;
+import remote.ExtratoPontosUCService;
+import remote.ExtratoPontosUCServiceLocator;
+import remote.Movimentacao;
+
 
 @SessionScoped
 @ManagedBean(name = "extratoUC")
 public class ExtratoUC {
 
+	private String login;
+	private String senha;
+	private Integer idSessao;
+	private List<Movimentacao> movimentacao;
+
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String logout() {
+		try {
+			ExtratoPontosUCService service = new ExtratoPontosUCServiceLocator();
+			ExtratoPontosUC proxy = service.getExtratoPontosUCPort();
+			proxy.logout(idSessao);
+		} catch (RemoteException e) {
+			FacesContext.getCurrentInstance().addMessage("login",
+					new FacesMessage("Sessão inválida!"));
+			return "index";
+		} catch (ServiceException e) {
+			FacesContext.getCurrentInstance().addMessage("login",
+					new FacesMessage("Problemas com o acesso ao servidor!"));
+			return "index";
+		}
+		return "index";
+	}
+	
+	public String logar(){
+		try {
+			ExtratoPontosUCService service = new ExtratoPontosUCServiceLocator();
+			ExtratoPontosUC proxy = service.getExtratoPontosUCPort();
+			idSessao = proxy.login(login, senha);
+		} catch (RemoteException e) {
+			FacesContext.getCurrentInstance().addMessage("login",
+					new FacesMessage("Usuário/Senha inválidos!"));
+			return "index";
+		} catch (ServiceException e) {
+			FacesContext.getCurrentInstance().addMessage("login",
+					new FacesMessage("Problemas com o acesso ao servidor!"));
+			return "index";
+		}
+		return "listProdutos";
+	}
+	
+	public List<Movimentacao> getMovimentacao() {
+		try {
+			ExtratoPontosUCService service = new ExtratoPontosUCServiceLocator();
+			ExtratoPontosUC proxy = service.getExtratoPontosUCPort();
+			
+			Movimentacao[] arrMovimentacao = proxy.extratoPontos(idSessao);
+			
+			movimentacao = new ArrayList<Movimentacao>(arrMovimentacao.length);
+			for (Movimentacao movimento : arrMovimentacao) {
+				movimentacao.add(movimento);
+			}
+		} catch (RemoteException e) {
+			FacesContext.getCurrentInstance().addMessage("login",
+					new FacesMessage("Sessão inválida!"));
+		} catch (ServiceException e) {
+			FacesContext.getCurrentInstance().addMessage("login",
+					new FacesMessage("Problemas com o acesso ao servidor!"));
+		}
+		return movimentacao;
+	}
+	
+	public void setMovimentacao(List<Movimentacao> movimentacao) {
+		this.movimentacao = movimentacao;
+	}
+	
 }
--- ExtratoClienteJSF/src/controle/GastarPontosUC.java
+++ ExtratoClienteJSF/src/controle/GastarPontosUC.java
@@ -1,10 +1,62 @@
 package controle;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIData;
+import javax.faces.context.FacesContext;
+import javax.xml.rpc.ServiceException;
+
+import remote.Produto;
+import remote.ProdutosUC;
+import remote.ProdutosUCService;
+import remote.ProdutosUCServiceLocator;
 
 @SessionScoped
 @ManagedBean(name = "gastarPontosUC")
 public class GastarPontosUC {
+	
+	private List<Produto> produtos;
+    private UIData selec;
+
+	public GastarPontosUC() {
+		try {
+			ProdutosUCService service = new ProdutosUCServiceLocator();
+			ProdutosUC proxy = service.getProdutosUCPort();
+			
+			Produto[] arrProduto = proxy.listar();
+			
+			produtos = new ArrayList<Produto>(arrProduto.length);
+			for (Produto produto : arrProduto) {
+				produtos.add(produto);
+			}
+		} catch (RemoteException e) {
+			FacesContext.getCurrentInstance().addMessage("login",
+					new FacesMessage("Sessão inválida!"));
+		} catch (ServiceException e) {
+			FacesContext.getCurrentInstance().addMessage("login",
+					new FacesMessage("Problemas com o acesso ao servidor!"));
+		}
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+    public UIData getSelec() {
+        return selec;
+    }
+
+    public void setSelec(UIData selec) {
+        this.selec = selec;
+    }
 
 }
